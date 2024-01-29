e637a3b3395ce90d351b6c61b17ac94d911c9b5d
Silvia-Wachira <slywachira96@gmail.com>
2024-01-29 23:53:08 +0300

    initialize the project

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-import Home from "./pages/Home.js"
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import './App.css';
+// import Home from "./pages/Home.js"
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path = "/" element ={<Home/>}/>
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
+
+// export default App;
+import React from 'react'
+import Sidebar from './Components/Sidebar'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path = "/" element ={<Home/>}/>
-      </Routes>
-    </BrowserRouter>
-  );
+    <div>
+      <Sidebar />
+    </div>
+  )
 }
 
-export default App;
+export default App
