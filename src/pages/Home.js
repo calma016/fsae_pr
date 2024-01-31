9f062002ead3241daf27852e7b550f6c6b2e1fc1
Kevin Tran <kevintran1034@Kevins-MacBook-Pro.local>
2024-01-31 12:02:09 -0500

    added picture files

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,39 @@
 import React from "react";
 
+import user_icon from '../Assets/user.svg'
+import email_icon from '../Assets/mail.svg'
+import password_icon from '../Assets/lock.svg'
+
+
 const Home = () => {
-	   return(
-			  <div>
-			  </div>
-	   )
+	return (
+		<div>
+			<div className='container'>
+				<div className='header'>
+					<div className="test">Sign Up</div>
+					<div className="underline"></div>
+				</div>
+				<div className="inputs">
+					<div className="input">
+						<img src={user_icon} alt="" />
+						<input placeholder="Username" type="text" />
+					</div>
+					<div className="input" >
+						<img src={email_icon} alt="" />
+						<input placeholder="Email" type="email" />
+					</div>
+					<div className="input">
+						<img src={password_icon} alt="" />
+						<input placeholder="Password" type="password" />
+					</div>
+				</div>
+				<div className="forgot-password">Lost Password? <span>Click Here</span></div>
+				<div className="submit-container">
+					<div className="submit">Sign Up</div>
+					<div className="submit">Login</div>
+				</div>
+			</div>
+		</div>
+	)
 }
 export default Home;
