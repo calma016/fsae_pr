26d04992c16ea5fa0debfa9b82199893f088cc27
Silvia-Wachira <slywachira96@gmail.com>
2024-02-12 16:26:16 +0300

    create and style the logos

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -6,6 +6,7 @@ import { AiOutlineHome } from "react-icons/ai";
 import { MdOutlineShoppingCart } from "react-icons/md";
 import { FaSackDollar } from "react-icons/fa6";
 import { MdOutlineSettings } from "react-icons/md";
+import  Logo  from "../assets/logo/pantherlogo.png"
 
 
 function Sidebar() {
@@ -18,7 +19,7 @@ function Sidebar() {
   return (
     <div className="flex relative" >
       {/* minisidebar */}
-      <div className={isShowingSidebar ? "hidden" : 'absolute flex flex-col items-center top-0 bottom-0 left-0 h-screen bg-purple-900 text-[#ffff] w-[50px] lg:hidden p-2 '}>
+      <div className={isShowingSidebar ? "hidden" : 'absolute flex flex-col items-center top-0 bottom-0 left-0 h-screen bg-purple-900 text-[#ffff] w-[50px] lg:hidden px-2 pt-4'}>
       <div>
             <div className='flex items-center justify-between '>
               {/* <div><h1>L</h1></div> */}
@@ -29,10 +30,10 @@ function Sidebar() {
        </div>
           <div>
               <ul onClick={toggleSidebar} className='list-none'>
-                  <li className=' my-5 cursor-pointer'> <AiOutlineHome /> </li>
-                  <li className=' my-5 cursor-pointer'> < MdOutlineShoppingCart /></li>
-                  <li className=' my-5 cursor-pointer'> <FaSackDollar /></li>
-                  <li className=' my-5 cursor-pointer'> <MdOutlineSettings /></li>
+                  <li className=' my-5 gap-2 cursor-pointer'> <AiOutlineHome /> </li>
+                  <li className=' my-5 gap-2 cursor-pointer'> < MdOutlineShoppingCart /></li>
+                  <li className=' my-5 gap-2 cursor-pointer'> <FaSackDollar /></li>
+                  <li className=' my-5 gap-2 cursor-pointer'> <MdOutlineSettings /></li>
               </ul>
           </div>
 
@@ -41,7 +42,9 @@ function Sidebar() {
        <div className={isShowingSidebar ?  " w-72 bg-purple-900 h-screen text-white leading-loose max-h-fit px-10 pt-4 lg:hidden transition ease-in-out " : "hidden"} >
           <div>
             <div className='flex items-center justify-between '>
-              <div>Logo</div>
+              <div>
+                <img src={Logo} alt='Logo' className='w-[70px]'/>
+              </div>
               <div >
                 {isShowingSidebar ? 
                 <MdOutlineClose onClick={toggleSidebar} className='cursor-pointer'/>:
@@ -62,7 +65,9 @@ function Sidebar() {
 
        {/* Navbar */}
        <div className=' absolute top-0 left-0 right-0 w-100  bg-purple-900 text-white hidden lg:flex h-[50px] items-center justify-between px-4'>
-          <div><h1>logo</h1></div>
+          <div>
+            <img src={Logo} alt='Logo' className='w-[70px]'/>
+          </div>
           <div>
             <ul className='flex items-center gap-4'>
               <li className='cursor-pointer'><a>Home</a></li>
